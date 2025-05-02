871576f421b6a05413451347972e9ddaac41c995
hyuns54 <qkrgustmd54@naver.com>
2025-05-02 13:02:50 +0900

    Add project files.

---
+++ WindowsFormsApp_cal_test1/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace WindowsFormsApp_cal_test1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_result = new System.Windows.Forms.TextBox();
+            this.btn_1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox_result
+            // 
+            this.textBox_result.Location = new System.Drawing.Point(231, 36);
+            this.textBox_result.Multiline = true;
+            this.textBox_result.Name = "textBox_result";
+            this.textBox_result.Size = new System.Drawing.Size(297, 402);
+            this.textBox_result.TabIndex = 0;
+            // 
+            // btn_1
+            // 
+            this.btn_1.Location = new System.Drawing.Point(287, 232);
+            this.btn_1.Name = "btn_1";
+            this.btn_1.Size = new System.Drawing.Size(75, 23);
+            this.btn_1.TabIndex = 1;
+            this.btn_1.Text = "button1";
+            this.btn_1.UseVisualStyleBackColor = true;
+            this.btn_1.Click += new System.EventHandler(this.btn_1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_1);
+            this.Controls.Add(this.textBox_result);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_result;
+        private System.Windows.Forms.Button btn_1;
+    }
+}
+
