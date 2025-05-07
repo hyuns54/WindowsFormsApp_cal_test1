c9f9780b4586773d4bbc9239f985e7c2b66d1ebd
hyuns54 <qkrgustmd54@naver.com>
2025-05-07 09:39:44 +0900

    변수 설정

--- WindowsFormsApp_cal_test1/Form1.cs
+++ WindowsFormsApp_cal_test1/Form1.cs
@@ -12,9 +12,16 @@ namespace WindowsFormsApp_cal_test1
 {
     public partial class Form1 : Form
     {
+            double cValue = 0; // cVALUE라는 변수에 0 대입
+            string cOperation = ""; // cOperation이라는 변수에 "" 대입
+        bool isOperationClicked = false; // 연산자가 클릭되었는지 확인하는 변수
+
         public Form1()
         {
             InitializeComponent();
+            display.Text = "0";
+
+        }
         }
 
         private void btn_1_Click(object sender, EventArgs e)
